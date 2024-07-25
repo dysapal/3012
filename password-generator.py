2cdee8920dae37f626673f1aad076a7ea1da7376
dysapal <oyalvarez@gmail.com>
2024-07-25 03:22:36 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,1 +1,8 @@
-
+import random
+caracter = "ABCDEFGHIJKLMNOPQRVXZTW/&%$#"
+longitud = int(input("introduce una longitud de contraseña:"))
+password = ""
+for i in range(longitud):
+    x =random.choice(caracter)
+    password += x   
+print(password)
